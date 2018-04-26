2b76eb9b2f1fa39e8a2ce54f677f530872c3f0cd
souths <2272230377@qq.com>
2018-04-27 00:23:27 +0800

    提交

---
+++ SmileVue/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app" class="van-hairline--surround">
+    <div class="null"></div><router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+.null{
+  height: 1rem;
+  background-color:#e501ad;
+}
+</style>
