ed2e778a8ae1568c791134be4ef9f961d66216bd
souths <2272230377@qq.com>
2018-05-10 21:50:13 +0800

    vconsole

--- SmileVue/src/main.js
+++ SmileVue/src/main.js
@@ -8,7 +8,7 @@ import api from './api'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'swiper/dist//css//swiper.css'
 import {Button, Row, Col,Swipe,SwipeItem,Lazyload} from 'vant'
-//require('./assets/util/vconsole.js')
+require('./assets/util/vconsole.js')
 Vue.prototype.$http = api;
 Vue.use(Button).use(Row).use(Col).use(Swipe).use(SwipeItem).use(Lazyload).use(VueAwesomeSwiper, /* { default global options } */)
 
