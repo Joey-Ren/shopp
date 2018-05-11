0b3cfd4bde92c87f05a38140380c6eb35f11d381
souths <2272230377@qq.com>
2018-05-12 00:00:35 +0800

    抽取楼层公共组件  跟watch的使用

--- SmileVue/src/components/pages/ShoppingMall.vue
+++ SmileVue/src/components/pages/ShoppingMall.vue
@@ -61,46 +61,12 @@
       </div>
     </div>
     <!-- 商品楼层 -->
-    <div class="floor" v-for="(floor,index) in floors" :key="index">
-      <p>
-        <span>{{index+1}}F</span> {{floor.floorName}}</p>
-      <div class="floord">
-        <div class="floord-h">
-          <div class="f-left">
-            <router-link :to="{'name':'gooddetails',params:{goodId:floor.floor[0].goodsId}}">
-              <img v-lazy="floor.floor[0].image" alt="">
-            </router-link>
-          </div>
-          <div class="f-right">
-            <div>
-              <router-link :to="{'name':'gooddetails',params:{goodId:floor.floor[1].goodsId}}">
-                <img v-lazy="floor.floor[1].image" alt="">
-              </router-link>
-
-            </div>
-            <div>
-              <router-link :to="{'name':'gooddetails',params:{goodId:floor.floor[2].goodsId}}">
-                <img v-lazy="floor.floor[2].image" alt="">
-              </router-link>
-            </div>
-          </div>
-        </div>
-        <div class="floord-b">
-          <div v-for="(floo,index) in floor.floor.slice(3)" :key="index">
-            <router-link :to="{'name':'gooddetails',params:{goodId:floo.goodsId}}">
-              <img v-lazy="floo.image" alt="">
-            </router-link>
-
-          </div>
-        </div>
-
-      </div>
-
-    </div>
+    <floor :floorData="floors"></floor>
   </div>
 </template>
 
 <script>
+import floor from '../component/floor'
 export default {
   name: "shoppingMall",
   data() {
@@ -121,6 +87,9 @@ export default {
         }
       }
     };
+  },
+  components:{
+    floor
   },
   mounted() {
     this.$http
@@ -230,65 +199,5 @@ export default {
     }
   }
 }
-.floor {
-  p {
-    text-align: center;
-    color: #e5017d;
-    margin: 5px 0;
-    font-size: 14px;
-    span {
-      display: inline-block;
-      width: 25px;
-      height: 25px;
-      color: #fff;
-      border-radius: 50%;
-      background-color: red;
-      line-height: 25px;
-    }
-  }
-  .floord-b {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    div {
-      border: 1px solid #eee;
-      width: 50%;
-      img {
-        width: 100%;
-        height: 100%;
-        vertical-align: middle;
-      }
-    }
-  }
-}
-.floord-h {
-  display: flex;
-  flex-direction: row;
-}
-.f-left {
-  border-bottom: 1px solid #eee;
-  border-right: 1px solid #eee;
-  background-color: #fff;
-  a {
-    display: inline-block;
-    width: 100%;
-    height: 100%;
-    img {
-      width: 100%;
-      height: 100%;
-      vertical-align: middle;
-    
-    }
-  }
-}
-.f-right {
-  div {
-    border-bottom: 1px solid #eee;
-    img {
-      width: 100%;
-      height: 100%;
-      vertical-align: middle;
-    }
-  }
-}
+
 </style>
