29b9beeda9eccdfd223cdd00c120bb2d2e7de773
souths <2272230377@qq.com>
2018-05-13 15:21:21 +0800

    路由懒加载组件

--- SmileVue/src/router/index.js
+++ SmileVue/src/router/index.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import ShoppingMall from '@/components/pages/ShoppingMall'
-import gooddetails from '@/components/pages/gooddetails'
-
+//正常路由加载组件
+//import ShoppingMall from '@/components/pages/ShoppingMall'
+//import gooddetails from '@/components/pages/gooddetails'
+// 使用路由懒加载来加载组件(提高加载速度)
+const ShoppingMall = () => import('@/components/pages/ShoppingMall')
+const gooddetails = () => import('@/components/pages/gooddetails')
 Vue.use(Router)
 
 export default new Router({
