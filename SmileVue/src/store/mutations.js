b63210ba32fcda87f9eb5a3c83bd2e155a22e024
souths <2272230377@qq.com>
2018-05-19 23:27:59 +0800

    store中的state从mutations中分离

--- SmileVue/src/store/mutations.js
+++ SmileVue/src/store/mutations.js
@@ -1,27 +1,24 @@
-import {SHOW_LOADING,HIDE_LOADING} from './types'
+import {
+  SHOW_LOADING,
+  HIDE_LOADING
+} from './types'
 import getters from './getters'
 import Vue from 'vue'
-
-const state = {
-    isLoading :false
-}
+import state from './state'
 
 const mutations = {
-    [SHOW_LOADING](state,msg){
-            console.log(state,'show_state');
-            console.log(msg,'show_msg');
-            state.isLoading = msg
-    },
-    [HIDE_LOADING](state,msg){
-        console.log(state,'hide_state');
-        console.log(msg,'hide_msg');
-        state.isLoading = msg
+  [SHOW_LOADING](state, msg) {
+    state.isLoading = msg
+  },
+  [HIDE_LOADING](state, msg) {
 
-    }
+    state.isLoading = msg
+
+  }
 }
 
 export default {
-    state,
-    mutations,
-    getters
+  state,
+  mutations,
+  getters
 }
