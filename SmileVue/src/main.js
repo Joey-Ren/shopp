a049e96f6e715f58dc9ab2ca9f2c8431e5fb09cd
souths <2272230377@qq.com>
2018-06-08 23:11:42 +0800

    注册

--- SmileVue/src/main.js
+++ SmileVue/src/main.js
@@ -4,21 +4,30 @@ import '@/assets/css/base.css'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import filters  from './filter/'
+import filters from './filter/'
 import api from './api'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'swiper/dist/css//swiper.css'
-import {Button, Row, Col,Swipe,SwipeItem,Lazyload,List} from 'vant'
+import {
+  Button,
+  Row,
+  Col,
+  Swipe,
+  SwipeItem,
+  Lazyload,
+  List,
+  NavBar,
+  Field
+} from 'vant'
 import alert from './components/plugin/alert'
 import loading from './components/loading/'
 import store from './store'
 require('./assets/util/vconsole.js')
 Vue.prototype.$http = api;
-Vue.use(Button).use(Row).use(Col).use(Swipe).use(SwipeItem).use(Lazyload).use(VueAwesomeSwiper).use(List).use(loading).use(alert)
-
+Vue.use(Button).use(Row).use(Col).use(Swipe).use(SwipeItem).use(Lazyload).use(VueAwesomeSwiper).use(List).use(loading).use(alert).use(NavBar).use(Field)
 //注入全局过滤器
-Object.keys(filters).forEach((key)=>{
-  Vue.filter(key,filters[key])
+Object.keys(filters).forEach((key) => {
+  Vue.filter(key, filters[key])
 })
 Vue.config.productionTip = false
 
@@ -27,6 +36,8 @@ new Vue({
   store,
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
--- SmileVue/src/router/index.js
+++ SmileVue/src/router/index.js
@@ -4,8 +4,12 @@ import Router from 'vue-router'
 //import ShoppingMall from '@/components/pages/ShoppingMall'
 //import gooddetails from '@/components/pages/gooddetails'
 // 使用路由懒加载来加载组件(提高加载速度)
-const ShoppingMall = () => import('@/components/pages/ShoppingMall')
-const gooddetails = () => import('@/components/pages/gooddetails')
+const ShoppingMall = () =>
+  import ('@/components/pages/ShoppingMall')
+const gooddetails = () =>
+  import ('@/components/pages/gooddetails')
+const Register = () =>
+  import ('@/components/pages/Register')
 Vue.use(Router)
 
 export default new Router({
@@ -18,7 +22,11 @@ export default new Router({
       path: '/gooddetails/:goodId',
       name: 'gooddetails',
       component: gooddetails
-
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
     }
   ]
 })
