bd85895c4b5f9a441e0ba072bfe440e0ffdef7bf
souths <2272230377@qq.com>
2018-07-17 21:22:10 +0800

    商品类别交互效果

--- SmileVue/src/components/pages/CategoryList.vue
+++ SmileVue/src/components/pages/CategoryList.vue
@@ -6,7 +6,11 @@
     <div>
       <van-row>
         <van-col :span="6">
-          左侧导航
+          <div id="leftNav">
+            <ul>
+              <li v-for="(item,index) in category" @click="activeClick(index)" :class="{categoryActive:categoryIndex===index}" :key="index">{{item.MALL_CATEGORY_NAME}}</li>
+            </ul>
+          </div>
         </van-col>
         <van-col :span="18">
           右侧导航
@@ -20,11 +24,15 @@
   export default {
     data() {
       return {
-          category:[]
+          category:[],
+          categoryIndex:'',
       }
     },
     mounted() {
       this.getCategoryList()
+      let winHeight = document.documentElement.clientHeight
+      document.getElementById('leftNav').style.height = winHeight-67+'px'
+      
     },
     methods: {
       async getCategoryList() {
@@ -35,12 +43,16 @@
         } = await this.$http.getCategoryList()
         if(data.code==200){
             console.log('数据请求成功');
-            this.category = data.data
+            this.category = data.message
+
             
         }else{
             console.log('数据请求失败');
             
         }
+      },
+      activeClick(index){
+        this.categoryIndex = index
       }
     }
 
@@ -50,5 +62,14 @@
 
 <style scoped>
 
-
+#leftNav ul li {
+    line-height: 0.8rem;
+    border-bottom:1px solid #E4E7ED;
+    padding:1px;
+    font-size:0.16rem;
+    text-align: center;
+}
+.categoryActive{
+    background-color: #fff;
+}
 </style>
